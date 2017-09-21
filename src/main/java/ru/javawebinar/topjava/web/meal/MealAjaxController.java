2714fdd9d85ea0bdb0e6f89b57c7975809d1bf12
vasiliyeskin <eskin@rf.unn.ru>
2017-09-21 16:05:14 -0600

    HW09

--- src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -1,15 +1,21 @@
 package ru.javawebinar.topjava.web.meal;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.StringJoiner;
 
 @RestController
 @RequestMapping(value = "/ajax/profile/meals")
@@ -28,14 +34,22 @@ public class MealAjaxController extends AbstractMealController {
     }
 
     @PostMapping
-    public void createOrUpdate(@RequestParam("id") Integer id,
-                               @RequestParam("dateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime,
-                               @RequestParam("description") String description,
-                               @RequestParam("calories") int calories) {
-        Meal meal = new Meal(id, dateTime, description, calories);
-        if (meal.isNew()) {
-            super.create(meal);
+    public ResponseEntity<String> createOrUpdate(MealTo mealTo, BindingResult result) {
+        if (result.hasErrors()) {
+            StringJoiner joiner = new StringJoiner("<br>");
+            result.getFieldErrors().forEach(
+                    fe -> { String msg = fe.getDefaultMessage();
+                        if (!msg.startsWith(fe.getField())) {msg = fe.getField() + ' ' + msg;}
+                        joiner.add(msg);});
+            return new ResponseEntity<>(joiner.toString(), HttpStatus.UNPROCESSABLE_ENTITY);
         }
+
+        if (mealTo.isNew()) {
+            super.create(MealsUtil.createNewFromTo(mealTo));
+        } else {
+            super.update(mealTo, mealTo.getId());
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Override
--- src/main/webapp/resources/js/mealDatatables.js
+++ src/main/webapp/resources/js/mealDatatables.js
@@ -2,12 +2,13 @@ var ajaxUrl = "ajax/profile/meals/";
 var datatableApi;
 
 function updateTable() {
-    $.ajax({
-        type: "POST",
-        url: ajaxUrl + "filter",
-        data: $("#filter").serialize(),
-        success: updateTableByData
-    });
+    /*    $.ajax({
+            type: "POST",
+            url: ajaxUrl + "filter",
+            data: $("#filter").serialize(),
+            success: updateTableByData
+        });*/
+    $.get(ajaxUrl, updateTableByData);
 }
 
 function clearFilter() {
@@ -17,11 +18,21 @@ function clearFilter() {
 
 $(function () {
     datatableApi = $("#datatable").DataTable({
+        "ajax": {
+            "url": ajaxUrl,
+            "dataSrc": ""
+        },
         "paging": false,
         "info": true,
         "columns": [
             {
-                "data": "dateTime"
+                "data": "dateTime",
+                "render": function (date, type, row) {
+                    if (type === "display") {
+                        return date.replace("T", " ");
+                    }
+                    return date;
+                }
             },
             {
                 "data": "description"
@@ -30,12 +41,14 @@ $(function () {
                 "data": "calories"
             },
             {
-                "defaultContent": "Edit",
-                "orderable": false
+                "orderable": false,
+                "defaultContent": "",
+                "render": renderEditBtn
             },
             {
-                "defaultContent": "Delete",
-                "orderable": false
+                "orderable": false,
+                "defaultContent": "",
+                "render": renderDeleteBtn
             }
         ],
         "order": [
@@ -43,7 +56,15 @@ $(function () {
                 0,
                 "desc"
             ]
-        ]
+        ],
+        "createdRow": function (row, data, dataIndex) {
+            if (data.exceed) {
+                $(row).addClass("exceeded");
+            }
+            else {
+                $(row).addClass("normal");
+            }
+        },
+        "initComplete": makeEditable
     });
-    makeEditable();
 });
