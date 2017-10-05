bf5e2b9766355cc11213e72f152272ddabf06b9d
vasiliyeskin <eskin@rf.unn.ru>
2017-10-05 17:19:22 -0600

    11 12 fix

--- src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.ErrorType;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import javax.validation.ConstraintViolationException;
@@ -14,6 +15,7 @@ import java.time.Month;
 import java.util.Arrays;
 
 import static java.time.LocalDateTime.of;
+import static org.hamcrest.core.StringContains.containsString;
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
@@ -64,7 +66,9 @@ public abstract class AbstractMealServiceTest extends AbstractServiceTest{
     @Test
     public void testUpdateNotFound() throws Exception {
         thrown.expect(NotFoundException.class);
-        thrown.expectMessage("Not found entity with id=" + MEAL1_ID);
+        thrown.expectMessage(containsString(ErrorType.DATA_NOT_FOUND.name()));
+        thrown.expectMessage(containsString(NotFoundException.NOT_FOUND_EXCEPTION));
+        thrown.expectMessage(containsString(String.valueOf(MEAL1_ID)));
         service.update(MEAL1, ADMIN_ID);
     }
 
